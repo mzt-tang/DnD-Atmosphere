91029420c6a24258dfbfeb4f60b26baf2007d610
Michael Tang <tangmich1@ecs.vuw.ac.nz>
2021-09-07 17:30:55 +1200

    removed unless import.

--- app/navigation/BottomTabNavigation.tsx
+++ app/navigation/BottomTabNavigation.tsx
@@ -3,11 +3,10 @@ import React from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LibraryScreen from "../screens/LibraryScreen";
 import HomeScreen from "../screens/HomeScreen";
-import {NavigationContainer} from "@react-navigation/native";
 
 const Tab = createBottomTabNavigator();
 
-export default function BottomTabNavigation({navigation}: any) {
+export default function BottomTabNavigation() {
     return (
         <Tab.Navigator initialRouteName={"Home"}>
             <Tab.Screen name="Home" component={HomeScreen}/>
