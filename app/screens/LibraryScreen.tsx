a4462260d519b1bb86c16ce331188f32c8668e69
Christopher Russell <russelchri@ecs.vuw.ac.nz>
2021-09-13 21:28:27 +1200

    Added scrolling to library

--- app/screens/LibraryScreen.tsx
+++ app/screens/LibraryScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Image, ImageBackground, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+import {Image, ImageBackground, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
 import {SearchBar} from "react-native-elements"
 
 export default function LibraryScreen() {
@@ -8,14 +8,16 @@ export default function LibraryScreen() {
         console.log("This would take you to a playlist");
     }
 
+    //Partially temporary, need to get data from database instead
     let playlists = [];
     let couple = [];
-    for (let i = 0; i < 6; i++) {
+    const playlistNum = 20;
+    for (let i = 0; i < playlistNum; i++) {
         couple.push({
             title: "Tavern",
             sourceImage: "../assets/images/tavern.jpg",
         });
-        if (i%2==1) { //Watch out if no. of playlists is odd
+        if (i%2===1 || i === playlistNum-1) { //Watch out if no. of playlists is odd
             playlists.push(couple);
             couple = [];
         }
@@ -31,23 +33,25 @@ export default function LibraryScreen() {
                 />
             </View>
 
-            {playlists.map(couple => 
+            <ScrollView style={styles.scroll}>
+                {playlists.map(couple => 
 
-                <View style={styles.couple}>
-                    {couple.map(playlist => 
+                    <View style={styles.couple}>
+                        {couple.map(playlist => 
 
-                        <TouchableOpacity style={styles.touchable} onPress={navToPlayList}>
-                            <ImageBackground 
-                                source={require("../assets/images/tavern.jpg")} //want this to be sourceImage but doesn't work
-                                style={styles.image}>
-                                
-                                <Text style={styles.imageText}>{playlist.title}</Text>
-                                
-                            </ImageBackground>
-                        </TouchableOpacity>
-                    )}
-                </View>
-            )}
+                            <TouchableOpacity style={styles.touchable} onPress={navToPlayList}>
+                                <ImageBackground 
+                                    source={require("../assets/images/tavern.jpg")} //want this to be sourceImage but doesn't work
+                                    style={styles.image}>
+                                    
+                                    <Text style={styles.imageText}>{playlist.title}</Text>
+                                    
+                                </ImageBackground>
+                            </TouchableOpacity>
+                        )}
+                    </View>
+                )}
+            </ScrollView>
 
         </SafeAreaView>
     );
@@ -57,7 +61,7 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: "#121212",
-        padding: 25,
+        paddingHorizontal: 25,
     },
     heading: {
         color: "#F4963F",
@@ -65,28 +69,33 @@ const styles = StyleSheet.create({
         marginTop: 35,
     },
     touchable: {
-        borderRadius: 100, //Doesn't work for some reason :(
+        borderRadius: 100, //Doesn't work for some reason :( tried it on image as well
     },
     image: {
-        width: 140,
-        height: 140,
-        marginRight: 25,
-        justifyContent: "flex-end",
+        width: 150,
+        height: 150,
+        marginRight: 10,
+        marginLeft: 0,
+        justifyContent: "flex-end", 
     },
     imageText: {
         color: "white",
         fontSize: 18,
     },
     couple: {
-        //backgroundColor: "red" //This is good for debugging
+        //backgroundColor: "red", //This is good for debugging
         width: "100%",
         flexDirection: "row",
-        marginBottom: 25,
+        marginBottom: 10,
+        justifyContent: "flex-start", //Center is better for different screen sizes but looks weird for odd numbers of playlists
     },
     searchBar: {
         backgroundColor: "white",
         marginBottom: 35,
         borderRadius: 10,
         paddingLeft: 5,
+    },
+    scroll: {
+        //backgroundColor: "yellow",
     }
 })
