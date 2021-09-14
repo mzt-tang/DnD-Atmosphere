686834bb9a7df76053ea92fd0ddba7e26fa2d867
Christopher Russell <russelchri@ecs.vuw.ac.nz>
2021-09-15 00:00:26 +1200

    Mostly finished soundboard screen minus audio playback

--- app/navigation/SoundboardNavigation.tsx
+++ app/navigation/SoundboardNavigation.tsx
@@ -7,11 +7,23 @@ import { RevealFromBottomAndroid } from "@react-navigation/stack/lib/typescript/
 
 const Stack = createStackNavigator();
 
+const SoundboadListOptions = {
+    
+}
+
 export default function SoundBoardNavigation() {
     return (
         <Stack.Navigator initialRouteName="Soundboards">
-            <Stack.Screen name="Soundboards" component={SoundboardListScreen} options={{headerShown:false}}/>
-            <Stack.Screen name="Soundboard" component={SoundboardScreen} />
+            <Stack.Screen name="Soundboards" component={SoundboardListScreen} options={{
+                headerShown: false, 
+            }} />
+            <Stack.Screen name="Soundboard" component={SoundboardScreen} options={{
+                headerTintColor: "#F4963F",
+                headerTitle: "",
+                headerStyle: {
+                    backgroundColor: "#121212",
+                    borderColor: "#121212",
+                } 
+            }} />
         </Stack.Navigator>
-    );
-}
+    )};
--- app/screens/SoundboardScreen.tsx
+++ app/screens/SoundboardScreen.tsx
@@ -1,12 +1,51 @@
 import React from "react";
-import {Button, SafeAreaView, StyleSheet} from "react-native";
+import {Button, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {AntDesign} from "@expo/vector-icons";
 
-export default function BoardScreen() {
+
+export default function BoardScreen({navigation}: any) {
+
+    function playSound() {
+        console.log("This would play a sound");
+    }
+
+    //Partially temporary, need to get data from database instead
+    let sounds = [];
+    let row = [];
+    const soundboardNum = 18;
+    for (let i = 0; i < soundboardNum; i++) {
+        row.push({
+            title: "Mug Clank",
+            duration: "0:02",
+            key: i,
+        });
+        if (i%3 === 2 || i === soundboardNum-1) { 
+            sounds.push(row);
+            row = [];
+        }
+    }
 
     return (
         <SafeAreaView style={styles.background}>
+            <Text style={styles.heading}>Tavern</Text>
+            <Text style={styles.subHeading}>Soundboard</Text>
 
-        </SafeAreaView>
+            <ScrollView>
+                {sounds.map(row => 
+
+                    <View style={styles.row} key={"r" + row[0].key}>
+                        {row.map(sound => 
+
+                            <TouchableOpacity onPress={playSound} style={styles.button} key={sound.key}>
+                                <Text style={styles.soundTitle}>{sound.title}</Text>
+                                <AntDesign style={styles.icon} name="caretright" color="white" size={45}/>
+                                <Text style={styles.soundDuration}>{sound.duration}</Text>
+                            </TouchableOpacity>
+                        )}
+                    </View>
+                )}
+            </ScrollView>
+        </SafeAreaView> 
     );
 }
 
@@ -14,5 +53,49 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: "#121212",
-    }
+        paddingHorizontal: 25,
+    },
+    heading: {
+        color: "#F4963F",
+        fontSize: 36,
+        alignSelf: "center",
+    },
+    subHeading: {
+        color: "white",
+        fontSize: 20,
+        alignSelf: "center",
+        marginTop: 5,
+        marginBottom: 20,
+    },
+    button: {
+        borderColor: "#F4963F",
+        borderWidth: 3,
+        borderRadius: 20,
+        width: 90,
+        height: 90,
+        marginRight: 20,
+    },
+    soundTitle: {
+        color: "white",
+        fontSize: 12,
+        marginLeft: 8,
+    },
+    soundDuration: {
+        color: "white",
+        fontSize: 12,
+        marginRight: 10,
+        alignSelf: "flex-end",
+    },
+    icon: {
+        marginTop: 3,
+        alignSelf: "center",
+
+    },
+    row: {
+        //backgroundColor: "red", //This is good for debugging
+        width: "100%",
+        flexDirection: "row",
+        marginBottom: 20,
+        justifyContent: "flex-start", //Center is better for different screen sizes but looks weird for odd numbers of soundboards
+    },
 })
