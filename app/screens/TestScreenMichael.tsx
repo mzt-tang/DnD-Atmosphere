2d935d877f7daf9fdbb90b94aced7ad24440aec0
Michael Tang <tangmich1@ecs.vuw.ac.nz>
2021-09-16 17:54:14 +1200

    Firebase test iteration 1 in testscreen

--- app/screens/TestScreenMichael.tsx
+++ app/screens/TestScreenMichael.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import firebase from "firebase/app";
 
 export default function TestScreenMichael({navigation}: any) {
 
-    const navToSettings = () => {
-        navigation.navigate("Settings")
+    let onChooseImagePress = async () => {
+        //let result = await ImagePicker.launchCameraAsync();
+        let result = await ImagePicker.launchImageLibraryAsync();
+        if (!result.cancelled) {
+            await uploadImage(result.uri);
+        }
+    }
+
+    let uploadImage = async(uri:any) => {
+        const response = await fetch(uri);
+        const blob = await response.blob();
+        const ref = firebase.storage().ref().child("my-image");
+        return ref.put(blob);
     }
 
     return (
@@ -12,16 +25,13 @@ export default function TestScreenMichael({navigation}: any) {
             <Text style={styles.title}>Firebase Storage</Text>
             <View>
                 <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>Take Photo</Text>
+                    <Text style={styles.buttonText}>Upload Audio</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>Record Video</Text>
+                    <Text style={styles.buttonText}>Select Audio</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>Pick a Photo</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>Pick a Video</Text>
+                    <Text style={styles.buttonText}>Play Audio</Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
