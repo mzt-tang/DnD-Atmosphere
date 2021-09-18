59b736d9c00ab6b1903465368492fd098e90ec3c
Christopher Russell <russelchri@ecs.vuw.ac.nz>
2021-09-18 20:21:46 +1200

    Music Player plays sound but this is temporary #4

--- app/screens/MusicPlayerScreen.tsx
+++ app/screens/MusicPlayerScreen.tsx
@@ -1,8 +1,36 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
 import {Button, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {Audio} from "expo-av";
+import { Slider } from 'react-native-elements';
+import TrackPlayer from "react-native-track-player";
 
 export default function MusicPlayerScreen({navigation}: any) {
+    const [sound, setSound] = React.useState<Audio.Sound>();
+
+    async function playSound() {
+        console.log('Playing Soundtrack');
+        sound?.replayAsync();
+    }
+
+    function sliderValueChanged(value: any){
+        console.log(value);
+        sound?.stopAsync();
+        //sound?.setPositionAsync(value);
+        sound?.playFromPositionAsync(value);
+    }
+
+    //Loads the sound (Sometimes it just stops working if so restart the server)
+    React.useEffect(() => {
+        async function loadSound() {
+            const { sound } = await Audio.Sound.createAsync(require('../assets/sounds/TavernsOfAzeroth.mp3'));
+            setSound(sound);
+            console.log('Soundtrack loaded');
+        }
+        
+        loadSound();
+    }, []);
+
     return (
         <SafeAreaView style={styles.background}>
             <Text style={styles.subHeading}>Playing from</Text>
@@ -14,11 +42,15 @@ export default function MusicPlayerScreen({navigation}: any) {
             <Text style={styles.artist}>Soundtrack Artist</Text>
 
             <View style={styles.seekBox}>
-                <View style={styles.seekBar}>
-                    <View style={styles.seekProgress} />
-                </View>
-                    <Text style={styles.currentTime}>4:54</Text>
-                    <Text style={styles.endTime}>13:29</Text> 
+                <Slider 
+                    value={0} 
+                    maximumValue={100}
+                    onSlidingComplete={value => sliderValueChanged(value)}
+                    thumbStyle={styles.sliderThumb} trackStyle={styles.sliderTrack} 
+                    minimumTrackTintColor="#F4963F"/>
+                
+                <Text style={styles.currentTime}>4:54</Text>
+                <Text style={styles.endTime}>13:24</Text> 
             </View>
 
             <View style={styles.icons}>
@@ -28,7 +60,7 @@ export default function MusicPlayerScreen({navigation}: any) {
                 <TouchableOpacity>
                     <MaterialIcons name="skip-previous" color="#F4963F" size={60}/>
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={playSound}>
                     <MaterialIcons name="play-circle-outline" color="#F4963F" size={60}/>    
                 </TouchableOpacity>
                 <TouchableOpacity>
@@ -76,17 +108,6 @@ const styles = StyleSheet.create({
         fontSize: 16,
         alignSelf: "center",
     },
-    seekBar: {
-        backgroundColor: "white",
-        height: 5,
-        borderRadius: 5,
-    }, 
-    seekProgress: {
-        backgroundColor: "#F4963F",
-        width: 100,
-        height: 5,
-        borderRadius: 5,
-    },
     seekBox: {
         alignSelf: "center",
         width: 320,
@@ -111,9 +132,19 @@ const styles = StyleSheet.create({
     icons: {
         marginLeft: 20,
         marginRight: 20,
-        marginTop: 40,
+        marginTop: 20,
         flexDirection: "row",
         justifyContent: "space-evenly",
         alignItems: "center",
+    },
+    sliderThumb: {
+        width: 8,
+        height: 8,
+        backgroundColor: "#F4963F",
+    },
+    sliderTrack: {
+        backgroundColor: "#F4963F",
+        height: 5,
+        borderRadius: 5,
     }
 })
