bc3814421613731f38270e30d67ee81378095bd2
Christopher Russell <russelchri@ecs.vuw.ac.nz>
2021-09-22 00:00:11 +1200

    Refactor - moved Sound into it's own file

--- app/components/Sound.tsx
+++ app/components/Sound.tsx
@@ -1,20 +1,56 @@
 import React from "react";
 import {Button, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {AntDesign} from "@expo/vector-icons";
+import {Audio} from "expo-av";
 
 
 export default function Sound(props : any) {
+    const [sound, setSound] = React.useState<Audio.Sound>();
+    const [duration, setDuration] = React.useState<number>(0);
 
-    function playSound() {
-        console.log("This would play a sound");
+    async function playSound() {
+        console.log('Playing Sound');
+        await sound?.replayAsync(); //if not null play sound 
     }
 
+    async function loadSound() {
+        console.log('Loading Sound');
+        const { sound , status} = await Audio.Sound.createAsync({uri: props.audioSource});
+
+        setSound(sound);
+        setDuration(status.durationMillis);
+    }
+
+
+    //Loads the sound (Sometimes it just stops working if so restart the server)
+    React.useEffect(() => {
+        loadSound();
+    }, []);
+
+    //Unloads the sound
+    React.useEffect(() => {
+        return sound
+          ? () => {
+              console.log('Unloading Sound');
+              sound.unloadAsync(); }
+          : undefined;
+      }, [sound]);
+
+    function millisToTimestamp(millis : number) {
+        const date = new Date(millis);
+        let seconds = "" + date.getSeconds();
+        if (date.getSeconds() < 10) {
+            seconds = "0" + seconds;
+        }
+        return date.getMinutes() + ":" + seconds;
+    }
+    
     return (
         <TouchableOpacity onPress={playSound} style={styles.button}>
             <Text style={styles.soundTitle}>{props.title}</Text>
             <AntDesign style={styles.icon} name="caretright" color="white" size={45}/>
-            <Text style={styles.soundDuration}>{props.duration}</Text>
-        </TouchableOpacity>
+            <Text style={styles.soundDuration}>{millisToTimestamp(duration)}</Text>
+        </TouchableOpacity> 
     );
 }
 
--- app/screens/SoundboardScreen.tsx
+++ app/screens/SoundboardScreen.tsx
@@ -1,61 +1,10 @@
-import React, { useEffect, useState } from "react";
-import {Button, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import {AntDesign} from "@expo/vector-icons";
-import {Audio} from "expo-av";
+import React from "react";
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from "react-native";
 
 import {QueueInfoContext} from "../components/QueueInfoContext";
 import MiniPlayer from "../components/MiniPlayer";
 import firebase from "firebase/app";
-
-function Sound(props : any) {
-    const [sound, setSound] = React.useState<Audio.Sound>();
-    const [duration, setDuration] = React.useState<number>(0);
-
-    async function playSound() {
-        console.log('Playing Sound');
-        await sound?.replayAsync(); //if not null play sound 
-    }
-
-    async function loadSound() {
-        console.log('Loading Sound');
-        const { sound , status} = await Audio.Sound.createAsync({uri: props.audioSource});
-
-        setSound(sound);
-        setDuration(status.durationMillis);
-    }
-
-
-    //Loads the sound (Sometimes it just stops working if so restart the server)
-    React.useEffect(() => {
-        loadSound();
-    }, []);
-
-    //Unloads the sound
-    React.useEffect(() => {
-        return sound
-          ? () => {
-              console.log('Unloading Sound');
-              sound.unloadAsync(); }
-          : undefined;
-      }, [sound]);
-
-    function millisToTimestamp(millis : number) {
-        const date = new Date(millis);
-        let seconds = "" + date.getSeconds();
-        if (date.getSeconds() < 10) {
-            seconds = "0" + seconds;
-        }
-        return date.getMinutes() + ":" + seconds;
-    }
-    
-    return (
-        <TouchableOpacity onPress={playSound} style={styles.button}>
-            <Text style={styles.soundTitle}>{props.title}</Text>
-            <AntDesign style={styles.icon} name="caretright" color="white" size={45}/>
-            <Text style={styles.soundDuration}>{millisToTimestamp(duration)}</Text>
-        </TouchableOpacity> 
-    );
-}
+import Sound from "../components/Sound";
 
 export default function BoardScreen(props: any) {
     const {queueInfo, setQueueInfo} = React.useContext(QueueInfoContext);
@@ -86,22 +35,6 @@ export default function BoardScreen(props: any) {
         console.log("Done loading");
         setSounds(tempSounds);
     }
-
-    //Partially temporary, need to get data from database instead
-    /*let sounds = [];
-    let row = [];
-    const soundboardNum = 18;
-    for (let i = 0; i < soundboardNum; i++) {
-        row.push({
-            title: "Mug Clank",
-            duration: "0:02",
-            key: i,
-        });
-        if (i%3 === 2 || i === soundboardNum-1) { 
-            sounds.push(row);
-            row = [];
-        }
-    } */
 
     return (
         <SafeAreaView style={styles.background}>
@@ -174,10 +107,9 @@ const styles = StyleSheet.create({
 
     },
     row: {
-        //backgroundColor: "red", //This is good for debugging
         width: "100%",
         flexDirection: "row",
         marginBottom: 20,
-        justifyContent: "flex-start", //Center is better for different screen sizes but looks weird for odd numbers of soundboards
+        justifyContent: "flex-start",
     },
 })
