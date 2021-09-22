98946bac878324ef8687d24a7de0a6e430fde614
Christopher Russell <russelchri@ecs.vuw.ac.nz>
2021-09-22 13:11:34 +1200

    Made BottomTab look nice #8
    Closes #8

--- app/navigation/BottomTabNavigation.tsx
+++ app/navigation/BottomTabNavigation.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {StyleSheet} from "react-native";
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LibraryScreen from "../screens/LibraryScreen";
@@ -6,6 +7,7 @@ import HomeScreen from "../screens/HomeScreen";
 import SoundBoardListScreen from "../screens/SoundboardListScreen";
 import TestScreenMichael from "../screens/TestScreenMichael"
 import Search from "../components/Search";
+import { MaterialIcons } from "@expo/vector-icons";
 
 
 const Tab = createBottomTabNavigator();
@@ -16,11 +18,40 @@ const Tab = createBottomTabNavigator();
  */
 export default function BottomTabNavigation() {
     return (
-        <Tab.Navigator initialRouteName={"Home"}>
-            <Tab.Screen name="Home" component={HomeScreen}/>
-            <Tab.Screen name="Library" component={LibraryScreen} />
-            <Tab.Screen name="Soundboards" component={SoundBoardListScreen} />
-            <Tab.Screen name={"TestScreen"} component={Search} />
+        <Tab.Navigator initialRouteName={"Home"} tabBarOptions={tabBarStyle}>
+            <Tab.Screen 
+                name="Home" 
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({color}) => <MaterialIcons name="home" color={color} size={35} />
+                }}/>
+            <Tab.Screen 
+                name="Soundtracks" 
+                component={LibraryScreen} 
+                options={{
+                    tabBarIcon: ({color}) => <MaterialIcons name="library-music" color={color} size={35} />
+                }}/>
+            <Tab.Screen 
+                name="Soundboards" 
+                component={SoundBoardListScreen} 
+                options={{
+                    tabBarIcon: ({color}) => <MaterialIcons name="grid-view" color={color} size={35} />
+                }}/>
+            <Tab.Screen 
+                name={"Search"} 
+                component={Search} 
+                options={{
+                    tabBarIcon: ({color}) => <MaterialIcons name="search" color={color} size={35} />
+                }}/>
         </Tab.Navigator>
-);
+    );
+}
+
+const tabBarStyle = {
+    activeTintColor: "#F4963F",
+    inactiveTintColor: "gray",
+    style: {
+        backgroundColor: "black", 
+        height: 50,
+    },
 }
