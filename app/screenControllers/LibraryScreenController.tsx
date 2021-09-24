a934edee40a2689ebe2e68e2a4bbcb3b10379615
Christopher Russell <russelchri@ecs.vuw.ac.nz>
2021-09-24 15:23:16 +1200

    Documented LibarayScreenController #12

--- app/screenControllers/LibraryScreenController.tsx
+++ app/screenControllers/LibraryScreenController.tsx
@@ -14,35 +14,45 @@ export default function LibraryScreenController({navigation} : any) {
         loadFromDatabase();
     }, []);
 
+    /**
+     * Loads the playlists from the databse, tranforms into objects which are then stored
+     * in the playlists state for the view to use.
+     */
     async function loadFromDatabase() {
         const playlistsCollection = await firebase.firestore().collection("soundtrack-categories").get();
 
         let i = 0;
+        // 2D list since the playlists need to be put in rows
         let tempPlaylists: any[] = [];
         let row: any[] = [];
         playlistsCollection.docs.forEach(doc => {
             const image = doc.data().image;
-            console.log(doc.id);
 
             row.push({
                 title: doc.id,
                 source: {uri: image},
                 key: i,
             });
+
+            // Evert 2nd iteration, push this row and start a new one
             if (i%2===1) {
                 tempPlaylists.push(row);
                 row = [];
             }
             i++;
         });
+
+        // This is so that the last playlist will still be added if there is an odd number of playlists
         if (row.length > 0) {
             tempPlaylists.push(row);
         }
 
         setPlaylists(tempPlaylists);
-        console.log("done loading", playlists);
     }
 
+    /**
+     * Transforms the playlist data into PlaylistButton components
+     */
     const playlistController = ({navigation}: any) => {
         return playlists.map(row =>
             <View style={{
@@ -51,6 +61,7 @@ export default function LibraryScreenController({navigation} : any) {
                 marginBottom: 10,
                 justifyContent: "flex-start"
             }} key={"r" + row[0].key}>
+
                 {row.map((playlist: any) =>
                     <PlaylistButton
                         source={playlist.source}
@@ -60,10 +71,15 @@ export default function LibraryScreenController({navigation} : any) {
                         navTo="Playlist"
                     />
                 )}
+
             </View>
         )
     }
 
+    /**
+     * Only returns a MiniPlayer component if the MiniPlayer has been activated yet
+     * @param navigation The stack navigation because MiniPlayer needs access to the MusicPlayerScreen
+     */
     const miniplayerController = ({navigation}: any) => {
         return queueInfo.mpActive && <MiniPlayer navigation={navigation}/>
     }
